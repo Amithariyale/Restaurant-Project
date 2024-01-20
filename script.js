23e9ab8c3e336b9e0c2555d08f0c811e1cff3b8d
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-20 14:37:48 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,35 +1,79 @@
 const foodItems = document.getElementById("food_items");
-
-document.addEventListener("DOMContentLoaded", () => {
-  getMenu();
-});
+let foodData;
+document.addEventListener("DOMContentLoaded", () => {});
 
 function getMenu() {
-  let foodData;
-  fetch(
+  return fetch(
     "https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json"
   )
-    .then((data) => data.json())
-    .then((data) => {
-      data.map((item) => {
-        foodItems.innerHTML +=
-         `
-        <div id="${item.id}" class="food_card">
-            <img src="${item.imgSrc}" alt="${item.name}">
-            <div class="card_text">
-                <div>
-                    <h3>${item.name}</h3>
-                    <p>$${item.price}/-</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-square-plus"></i>
-                </div>
-            </div>
-        </div>
-        `;
-      });
-    })
+    .then((response) => response.json())
+    .then((data) => showMenu(data))
     .catch((e) => console.log(e));
-
-  console.log(foodData);
 }
+
+function showMenu(data) {
+  foodData = data;
+  foodData.map((item) => {
+    foodItems.innerHTML += `
+      <div id="${item.id}" class="food_card">
+          <img src="${item.imgSrc}" alt="${item.name}">
+          <div class="card_text">
+              <div>
+                  <h3>${item.name}</h3>
+                  <p>$${item.price}/-</p>
+              </div>
+              <div>
+                  <i class="fa-solid fa-square-plus"></i>
+              </div>
+          </div>
+      </div>
+      `;
+  });
+}
+
+function takeOrder() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomBurgers = [];
+      for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * foodData.length);
+        randomBurgers.push(foodData[randomIndex]);
+      }
+      const order = { burgers: randomBurgers };
+      resolve(order);
+    }, 2500);
+  });
+}
+
+function orderPrep() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const orderStatus = { order_status: true, paid: false };
+      resolve(orderStatus);
+    }, 1500);
+  });
+}
+
+function payOrder(orderStatus) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      orderStatus = { ...orderStatus, paid: true };
+      resolve(orderStatus);
+    }, 1000);
+  });
+}
+
+function thankYouFnc() {
+  alert("Thank you for eating with us today!");
+}
+
+getMenu()
+  .then(() => takeOrder())
+  .then((order) => orderPrep(order))
+  .then((orderStatus) => payOrder(orderStatus))
+  .then((orderStatus) => {
+    if (orderStatus.paid) {
+      thankYouFnc();
+    }
+  })
+  .catch((err) => console.log(err));
